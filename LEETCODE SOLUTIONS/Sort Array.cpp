4d39a58a4d7d0da017ab14094df25cede94ae8de
Ayush Shah <95910417+AyushShah2003@users.noreply.github.com>
2023-03-01 12:25:27 -0500

    Commit

---
+++ LEETCODE SOLUTIONS/Sort Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    //using Merge Sort
+    vector<int> tmp;
+    void mergeSort(vector<int> &A, int begin, int end) {
+        if (begin + 1 >= end) return;
+        int mid = (begin + end) / 2, i = begin, j = mid, k = begin;
+        mergeSort(A, begin, mid);
+        mergeSort(A, mid, end);
+        while (i < mid || j < end) {
+            if (j >= end || (i < mid && A[i] < A[j])) tmp[k++] = A[i++];
+            else tmp[k++] = A[j++];
+        }
+        for (i = begin; i < end; ++i) A[i] = tmp[i];
+    }
+public:
+    vector<int> sortArray(vector<int>& A) {
+        tmp.assign(A.size(), 0);
+        mergeSort(A, 0, A.size());
+        return A;
+    }
+};
+
+
+      
