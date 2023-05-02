2c106b006f2f20f819e98e43acf31771b649f71c
Ayush Shah <95910417+AyushShah2003@users.noreply.github.com>
2023-05-02 03:12:36 -0400

    Commit

---
+++ LEETCODE SOLUTIONS/Sign of the Product of an Array .cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int negativeCount = 0;
+        for(int num : nums) {
+            if(num == 0) {
+                return 0;
+            } else if(num < 0) {
+                negativeCount++;
+            }
+        }
+        return negativeCount % 2 == 0 ? 1 : -1;
+    }
+};
